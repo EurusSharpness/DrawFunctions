4da330486c9f51eff59ff1bd046e623a432c9a0f
Same Eldin <samheldin.1999@gmail.com>
2020-11-05 10:26:22 +0200

    Add project files.

---
+++ DrawFunctions/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawFunctions
+{
+    public partial class Form1 : Form
+    {
+        MainClass Main;
+        public Form1()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+            InitializeComponent();
+        }
+
+        private void Refresher_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Main = new MainClass();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            Main.Draw(g);
+        }
+    }
+}
---
+++ DrawFunctions/MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawFunctions
+{
+    class MainClass
+    {
+        public MainClass()
+        {
+
+        }
+
+        public void Draw(Graphics g)
+        {
+
+        }
+    }
+}
